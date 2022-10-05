6141b33d1843a6ba65122f58ca413180fe80fc13
CyskoFR <franc.sko91@gmail.com>
2022-10-05 09:36:19 +0200

    add Pizza MVC

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -2,13 +2,15 @@
 {
     public class Pizza
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Picture { get; set; }
         public double Price { get; set; }
 
-        public Pizza(string name, string description, string picture, double price)
+        public Pizza(int id, string name, string description, string picture, double price)
         {
+            Id = id;
             Name = name;
             Description = description;
             Picture = picture;
